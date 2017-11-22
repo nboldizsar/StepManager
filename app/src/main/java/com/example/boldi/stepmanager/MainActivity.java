f106e4f70e2d594231f577e47f57aceae6c64ce1
boldi12@gmail.com <boldi12@gmail.com>
2017-11-22 19:53:32 +0100

    ActionBarButton add

--- app/src/main/java/com/example/boldi/stepmanager/MainActivity.java
+++ app/src/main/java/com/example/boldi/stepmanager/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -91,4 +92,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar1, menu);
+        return true;
+    }
 }
